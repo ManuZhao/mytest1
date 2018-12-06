54fa4dd80b1ddd498cfdbb101c9e065d08b93c95
ManuZhao <zhaojingbo@nantian.com.cn>
2018-12-06 17:23:12 +0800

    GitHub测试

--- src/cn/xx/learn/Test001.java
+++ src/cn/xx/learn/Test001.java
@@ -3,18 +3,55 @@
  */
 package cn.xx.learn;
 
+import com.starnet.springframework.beans.BeanUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+
 public class Test001 {
     public static void main(String[] args){
-        double interestTmp = 10000 * (Math.pow(1 + 0.01, (double) 45 / (double) 360) - 1);
-        double interestTmp1 = 10000 * (Math.pow(1 + 0.01, (double) 45 / (double) 365) - 1);
-        System.out.println(interestTmp);
-        System.out.println(interestTmp1);
+        B b = new B();
+        A a = new A();
+        BeanUtils.copyProperties(b, a);
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+    }
+}
+
+class A{
+    int a;
+    String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
+
+class B{
+    int b;
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
     }
 }
 
